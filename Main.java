c67e5958d7a998a874940991936a5b17ebe079c9
Ryan Bitrick <rbitrick@gmail.com>
2023-07-04 18:35:00 -0400

    Commit 2

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        ZonedDateTime time = ZonedDateTime.now(
+            ZoneId.systemDefault()
+        );
+
+        DateTimeFormatter dateformatted = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        String formattedDate = time.format(dateformatted);
+        System.out.println("Hello everyone! The current date and time is: " + formattedDate);
     }
 }
